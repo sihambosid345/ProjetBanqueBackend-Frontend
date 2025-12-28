4066734c0536fca38563e206a591078013aef1e3
sihambosid345 <sihambouzid2024@gmail.com>
2025-12-28 02:32:53 +0100

    Delete src directory

--- src/app/app.routes.server.ts
+++
@@ -1,5 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  // même routes que app.routes.ts pour SSR
-];
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-
-
-export const environment = {
-  production: false,
-  backendHost : "http://localhost:8084"
-};
