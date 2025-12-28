4951349ddddb90c926cd35ccb5ff341fd3c1ddc1
sihambosid345 <sihambouzid2024@gmail.com>
2025-12-28 02:21:06 +0100

    Delete src directory

--- src/app/app.config.server.ts
+++
@@ -1,8 +0,0 @@
-// src/app/app.config.server.ts
-import { ValueProvider } from '@angular/core';
-
-export const API_URL = 'http://localhost:8080/api';
-
-export const serverConfigProviders: ValueProvider[] = [
-  { provide: 'API_URL', useValue: API_URL }
-];
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  backendHost: 'http://localhost:8084'
-};
